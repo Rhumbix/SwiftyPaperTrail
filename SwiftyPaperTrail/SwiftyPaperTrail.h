d7d9adea54eef08585d16222ece5a051c4a330eb
Majd Murad <murad.majd@gmail.com>
2016-11-30 11:58:20 -0800

    Initial Commit

---
+++ SwiftyPaperTrail/SwiftyPaperTrail.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyPaperTrail.h
+//  SwiftyPaperTrail
+//
+//  Created by Majd Murad on 11/30/16.
+//  Copyright © 2016 Rhumbix, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyPaperTrail.
+FOUNDATION_EXPORT double SwiftyPaperTrailVersionNumber;
+
+//! Project version string for SwiftyPaperTrail.
+FOUNDATION_EXPORT const unsigned char SwiftyPaperTrailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyPaperTrail/PublicHeader.h>
+
+
