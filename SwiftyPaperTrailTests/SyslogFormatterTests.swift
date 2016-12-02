21e36b35625c08efa40778326ec27920bf90b65d
Majd Murad <murad.majd@gmail.com>
2016-12-02 14:51:35 -0800

    Added new SyslogFormat tests

--- SwiftyPaperTrailTests/SyslogFormatterTests.swift
+++ SwiftyPaperTrailTests/SyslogFormatterTests.swift
@@ -15,8 +15,34 @@ class SyslogFormatterTests: XCTestCase {
         XCTAssert(type(of: formattedString) == String.self)
     }
     
-    func testFormatOfMessage() {
+    func testDefaultSyslogFormat() {
         let formattedString = SyslogFormatter.sharedInstance.formatLogMessage(message: "Testing Message")
+        let pattern = "<\\d{2}>.+ .+ .+:.{0,}"
+        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+        let matches = regex.matches(in: formattedString, options: [], range: NSRange(location: 0, length: formattedString.characters.count))
+        XCTAssertFalse(matches.isEmpty)
+    }
+    
+    func testDifferentMachineName() {
+        let customMachineName = "My-Custom-Machine-Name"
+        SyslogFormatter.sharedInstance.machineName = customMachineName
+        let formattedString = SyslogFormatter.sharedInstance.formatLogMessage(message: "Testing Machine Name Change")
+        let pattern = "<\\d{2}>.+ \(customMachineName) .+:.{0,}"
+        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+        let matches = regex.matches(in: formattedString, options: [], range: NSRange(location: 0, length: formattedString.characters.count))
+        
+        XCTAssertFalse(matches.isEmpty, "Machine name did not match in pattern")
+    }
+    
+    func testDifferentProgramName() {
+        let customProgramName = "My-Custom-Program-Name"
+        SyslogFormatter.sharedInstance.programName = customProgramName
+        let formattedString = SyslogFormatter.sharedInstance.formatLogMessage(message: "Testing Program Name Change")
+        let pattern = "<\\d{2}>.+ .+ \(customProgramName):.{0,}"
+        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+        let matches = regex.matches(in: formattedString, options: [], range: NSRange(location: 0, length: formattedString.characters.count))
+        
+        XCTAssertFalse(matches.isEmpty, "Program name did not match in pattern")
     }
     
 }
